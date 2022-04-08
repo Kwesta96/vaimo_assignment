3e2ede010f2eb51d100adbeba9dc4fa3c920f3b3
Paa Kwesi Owusu-Ansah <kwesta96@gmail.com>
2022-04-08 23:38:21 +0200

    updated reviews

--- src/components/InfoBox/Ratings/Ratings.jsx
+++ src/components/InfoBox/Ratings/Ratings.jsx
@@ -5,6 +5,9 @@ const Ratings = ({rating}) => {
   return (
     <div className='ratings'>
       <Rating name="read-only" value={parseFloat(rating.rating)} precision={0.1} readOnly className='stars'/>
+      <span className='ratingNumber'>{rating.rating}</span>
+      <span className='reviews'>{rating.count} Reviews</span>
+      <span className='buyers'>{rating.total_buyers} buyers</span>
     </div>
   )
 }
